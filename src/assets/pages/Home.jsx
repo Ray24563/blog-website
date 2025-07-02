7d8a3a0c352d76499180606dc375a177a3938ce6
Bari <charlespalatino@gmail.com>
2025-07-02 10:02:45 +0800

    Implement post creation functionality

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,25 +1,69 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Home(){
 
   useEffect(() => {
-  document.title = "Just my Type";
-}, []);
+    document.title = "Just my Type";
+    localStorage.setItem("myPosts", JSON.stringify(posts));
+  }, []);
 
- const today = new Date();
-const dayName = today.toLocaleDateString('en-US', { weekday: 'long' });
+  // Day Greetings Logic
+  const today = new Date();
+  const dayName = today.toLocaleDateString('en-US', { weekday: 'long' });
 
-const greetingsByDay = {
-  Sunday: "Take time to rest and recharge — it's",
-  Monday: "A fresh start for new goals — it's",
-  Tuesday: "Keep the momentum going — it's",
-  Wednesday: "You're halfway through the week — it's",
-  Thursday: "The weekend is almost here — it's",
-  Friday: "Wrap up strong and smile — it's",
-  Saturday: "Relax and enjoy yourself — it's",
-};
+  const greetingsByDay = {
+    Sunday: "Take time to rest and recharge — it's",
+    Monday: "A fresh start for new goals — it's",
+    Tuesday: "Keep the momentum going — it's",
+    Wednesday: "You're halfway through the week — it's",
+    Thursday: "The weekend is almost here — it's",
+    Friday: "Wrap up strong and smile — it's",
+    Saturday: "Relax and enjoy yourself — it's",
+  };
 
-const dayGreetings = greetingsByDay[dayName] || "Thank you for using Just my Type!";
+  const dayGreetings = greetingsByDay[dayName] || "Thank you for using Just my Type!";
+
+  // Adding Post Logic
+  const [posts, setPost] = useState([]);
+  const [createTitle, setTitle] = useState("")
+  const [currentDate, setCurrentDate] = useState(
+      new Date(). toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+        })
+      );
+  const [createDesc, setCreateDesc] = useState("");
+  
+
+  function newTitle(event){
+    setTitle(event.target.value)
+  }
+
+  function newDesc(event){
+    setCreateDesc(event.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    const newPost = {
+      title: createTitle,
+      date: currentDate,
+      description: createDesc
+    }
+
+    setPost(p => [...p, newPost])
+    setTitle("")
+    setCreateDesc("")
+    setCurrentDate(useState(
+      new Date(). toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+        })
+      ));
+    }
 
   return (
     <>
@@ -31,13 +75,13 @@ const dayGreetings = greetingsByDay[dayName] || "Thank you for using Just my Typ
           <p className="mb-10 font-[sans-serif]">Hey there! Hope your day’s been kind to you. Welcome to <span className="custom-font">Just My Type</span> — your own little corner to jot down thoughts, memories, or whatever’s on your mind. Think of it like a digital diary that’s all yours, saved right here on your device. <span className="custom-font">Take a breath, take your time,</span> and <span className="custom-font">start writing</span>.</p>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <input className="bg-[#F2F4F3] mb-5 rounded-sm border border-black border-solid ps-3 pt-2 pb-2 font-[sans-serif]" placeholder="Enter Title" type="text"/>
+            <input value={createTitle} onChange={newTitle} className="bg-[#F2F4F3] mb-5 rounded-sm border border-black border-solid ps-3 pt-2 pb-2 font-[sans-serif]" placeholder="Enter Title" type="text"/>
           </div>
 
           <div>
-            <textarea className="resize-none mb-5 bg-[#F2F4F3] w-70 rounded-sm border border-black border-solid ps-3 pt-2 pb-2 font-[sans-serif]" placeholder="How are you feeling today?" type="text"/>
+            <textarea value={createDesc} onChange={newDesc} className="resize-none mb-5 bg-[#F2F4F3] w-70 rounded-sm border border-black border-solid ps-3 pt-2 pb-2 font-[sans-serif]" placeholder="How are you feeling today?" type="text"/>
           </div>
 
           <button className="hover:opacity-85 bg-[#5E503F] cursor-pointer text-white font-[sans-serif] rounded-sm ps-8 pe-8 pt-2 pb-2 transition-opacity duration-300">Post</button>
@@ -45,6 +89,13 @@ const dayGreetings = greetingsByDay[dayName] || "Thank you for using Just my Typ
 
       </div>
     </div>
+
+      <div>
+        <ul>
+          {posts.map((post, index) => 
+          <li key={index}>{post.title} - {post.date} - {post.description}</li>)}
+        </ul>
+      </div>
     </>
   )
 }
